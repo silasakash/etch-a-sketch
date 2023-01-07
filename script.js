4d304eaed0cf6a32a81915e6a04c977e47b36a9c
silasakash <akashsilas1@gmail.com>
2023-01-08 01:03:28 +0530

    Modify index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const color = document.getElementById("colorpicker");
-const rangeValue = document.getElementById("rangevalue");
-const sizeOfCanvas = document.getElementById("sizeofcanvas");
-const canvas = document.getElementById("canvas");
-const size = sizeOfCanvas.value;
+let color = "black";
+let rangeValue = document.getElementById("rangevalue");
+let sizeOfCanvas = document.getElementById("sizeofcanvas");
+let canvas = document.getElementById("canvas");
+let size = sizeOfCanvas.value;
 
 
 rangeValue.innerHTML = sizeOfCanvas.value;
@@ -15,11 +15,29 @@ function canvasElements(size) {
     canvas.style.gridTemplateRows = `repeat(${size},1fr)`;
 
     for (i=1;i<=size*size;i++) {
+
+        let count = 0;  
         const internalPixel = document.createElement('div');
         canvas.insertAdjacentElement("beforeend",internalPixel);
+        internalPixel.addEventListener("mouseover", pixelColor);
     }
 }
-
 function changeSize(input) {
     canvasElements(input);
+}
+function pixelColor() {
+    if (color == 'rainbow'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }    
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetCanvas() {
+    let pixels = canvas.querySelectorAll("div");
+    pixels.forEach((div) => div.style.backgroundColor = 'white');
 }
